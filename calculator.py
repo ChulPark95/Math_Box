9b300a60cd820ebe7812896ce7b8172c51d949b2
ChulPark95 <ironkite2728@naver.com>
2024-11-14 09:24:19 +0900

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -8,9 +8,8 @@ def subtract(a,b):
 def multiply(a,b):
     return a*b
 
-<<<<<<< HEAD
-def divide_premium(a,b):
-=======
-def divide_free(a,b):
->>>>>>> master
+def devide_new(a,b):
     return a/b
+
+def say_hello():
+    print("Hello")
